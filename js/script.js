ec35ea9a859a02585d4879f73f9838e8fc236c05
Ling Lu <jojoucsd@gmail.com>
2016-03-25 16:28:48 -0700

    added jeykll and new site is up

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	console.log('We in Business!');
+	$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		url: "https://api.instagram.com/v1/users/257398550/media/recent?access_token=257398550.9c8d6f2.773bc21a5fab48d58b69828c0b627611&count=9",
+		success: function (json) {
+			$.each(json.data, function () {
+				var thisdata = this;
+				$('#instagram-box').append("<img src='" + this.images.thumbnail.url + "'>");
+			});
+	    }
+	});
+
+	//google map
+	var map;
+	var marker;
+
+	function initMap() {
+	  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+	    center: {lat: 37.790841, lng: -122.401280},  
+	    zoom: 11,
+	    styles:[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}],
+	    mapTypeId: google.maps.MapTypeId.TERRAIN
+	  });
+	  var infoWindow = new google.maps.InfoWindow({map: map});
+
+	  // Try HTML5 geolocation.
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	      var pos = {
+	        lat: position.coords.latitude,
+	        lng: position.coords.longitude
+	      };
+	      infoWindow.setPosition(pos);
+	      infoWindow.setContent("You Are Here !");
+	      map.setCenter(pos);
+	    }, function() {
+	      handleLocationError(true, infoWindow, map.getCenter());
+	    });
+	  } else {
+	    // Browser doesn't support Geolocation
+	    handleLocationError(false, infoWindow, map.getCenter());
+	  }
+	}
+
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	  infoWindow.setPosition(pos);
+	  infoWindow.setContent(browserHasGeolocation ?
+	                        'Error: The Geolocation service failed.' :
+	                        'Error: Your browser doesn\'t support geolocation.');
+	}
+	initMap();
+});
